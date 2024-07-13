44c4d49cff693be1397aa79dda1d70fddd499999
Abhishek <goswamiabhi9729@gmail.com>
2024-07-13 22:36:42 +0530

    added some line of code

--- day1.js
+++ day1.js
@@ -1,7 +1,31 @@
+//Activity 1: Variable Declaration
 var num1 = 20;
 console.log(num1);
 let string1 = 'stringExample';
 console.log(string1);
+
+//Activity 2: Constant Declaration
 const boolValue = true;
 console.log(boolValue);
 
+//Activity 3: Data Types 
+console.log(typeof(num1));
+console.log(typeof(string1));
+console.log(typeof(boolValue));
+
+const arr = [2,3,4,5,6];
+console.log(typeof(arr));
+// console.log(arr[1]);
+
+let obj = {name: 'Abhi', age: 22};
+console.log(typeof(obj));
+
+//Activity 4: Reassigning Variables
+let variable2 = 223;
+console.log('old value: ' + variable2);
+variable2 = 224;
+console.log('new value: ' + variable2);
+
+//Activity 5: Understanding const keyword
+const constant1 = 10;
+//constant1 = 4; Gives error because we can not change constant values
