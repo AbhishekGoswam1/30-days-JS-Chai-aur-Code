b01efb1ce2aeb880a21d3ad9c87ad65c5f7d59a5
Abhishek <goswamiabhi9729@gmail.com>
2024-07-20 09:47:04 +0530

    day 7 and 8 files created

---
+++ day7.js
@@ -0,0 +1,14 @@
+// This is day 7 of "30 days JavaScript challenge"
+// and today's tasks/activities are on "Objects"
+
+
+// Activity 1: Object creation and access
+
+// 1. Create an object representing a book with properties like title, author, and year.
+
+let book = {
+    title: "Atomic Habits",
+    author: "James Clear",
+    year: 2018
+};
+console.log(book);
